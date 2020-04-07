002d7bd6c947e441431848ab5a5eff597c62b2dd
WhiteWolf96 <davidw2009@hotmail.com>
2020-04-07 13:48:34 +0100

    added merlin voice fade in instruction scene

--- Assets/Scripts/InstructionsManager.cs
+++ Assets/Scripts/InstructionsManager.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InstructionsManager : MonoBehaviour
 {
     [SerializeField] Rewired.Player gamePadController1, gamePadController2;
 
     [SerializeField] Animator fadeOut;
-    [SerializeField] GameObject check1, check2;
+    [SerializeField] AudioSource sound;
+    [SerializeField] Image check1, check2;
+
+    [Range(0, 1)] [SerializeField] float checkTransparency = 0.3f;
 
     private SceneChanger sceneChanger;
 
-    bool playGame = false;
+    bool isReady = false;
     
     // Start is called before the first frame update
     void Start()
@@ -27,20 +31,22 @@ public class InstructionsManager : MonoBehaviour
     void Update()
     {
 
-        if (playGame)
+        if (!isReady)
         {
-            check1.SetActive(true); // visual check for player 1
-            check2.SetActive(true); // visual check for player 2
-        } else
-        {
-            check1.SetActive(gamePadController1.GetButton("Jump")); // visual check for player 1
-            check2.SetActive(gamePadController2.GetButton("Jump")); // visual check for player 2
+            //check1.SetActive(gamePadController1.GetButton("Jump")); // visual check for player 1
+            //check2.SetActive(gamePadController2.GetButton("Jump")); // visual check for player 2
+            toggleConfirmation(check1, gamePadController1); // visual check for player 1
+            toggleConfirmation(check2, gamePadController2); // visual check for player 2
         }
 
         // Go To Game Scene
         if (gamePadController1.GetButton("Jump") && gamePadController2.GetButton("Jump") || Input.GetKeyDown(KeyCode.Return))
         {
-            playGame = true;
+            isReady = true;
+            toggleConfirmation(check1, null, true); // visual check for player 1
+            toggleConfirmation(check2, null, true); // visual check for player 2
+            IEnumerator fadeSound = AudioFadeOut.FadeOut(sound, 2.0f);
+            StartCoroutine(fadeSound);
             sceneChanger.goToScene(2, true);
 
         }
@@ -53,4 +59,30 @@ public class InstructionsManager : MonoBehaviour
             
     }
 
+    private void toggleConfirmation(Image check, Rewired.Player gamePadController, bool ready = false)
+    {
+        if (ready)
+        {
+            Color tempColor = check.color;
+            tempColor.a = 1f;
+            check.color = tempColor;
+        }
+        else
+        {
+            if (gamePadController.GetButton("Jump"))
+            {
+                Color tempColor = check.color;
+                tempColor.a = 1f;
+                check.color = tempColor;
+            }
+            else
+            {
+                Color tempColor = check.color;
+                tempColor.a = checkTransparency;
+                check.color = tempColor;
+            }
+        }
+        
+        
+    }
 }
